d007f054776c5448ce8ff16f6edbecc12b4ebaeb
YOGESH <yogesh000aryal@gmial.com>
2021-09-01 22:39:08 +0545

    this is third commit

--- src/components/News.js
+++ src/components/News.js
@@ -24,26 +24,26 @@ export class News extends Component {
       totalResults: 0,
     };
   }
-
   capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   async updateNews() {
-      this.props.setProgress(10);
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-      let data = await fetch(url);
-      this.props.setProgress(30);
-      let parsedData = await data.json();
-      this.props.setProgress(70);
-      this.setState({
-          articles: parsedData.articles,
-          totalResults: parsedData.totalResults,
-          loading: false,
-        });
-      this.props.setProgress(100);
+    this.props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    this.props.setProgress(30);
+    let parsedData = await data.json();
+    this.props.setProgress(70);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+    this.props.setProgress(100);
   }
-
+  
   async componentDidMount() {
+    document.title = `Peigon News | ${this.capitalizeFirstLetter(this.props.category)}`;
     this.updateNews();
   }
 
